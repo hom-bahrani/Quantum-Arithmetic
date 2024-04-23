d8505d1153330b30b3b4b8bd594fb74d8daa81cf
hom-bahrani <hab0184@gmail.com>
2024-04-23 10:54:37 +0100

    add logic for 1+1

--- ibm_qiskit/main.py
+++ ibm_qiskit/main.py
@@ -1,53 +1,57 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Import Qiskit
+from qiskit import QuantumCircuit, transpile
 from qiskit_aer import AerSimulator
-from qiskit.circuit import QuantumCircuit
-from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
-from qiskit_ibm_runtime import (
-    SamplerV2 as Sampler,
-    QiskitRuntimeService,
-    IBMRuntimeService,
-)
+from qiskit.visualization import plot_histogram, plot_state_city
+import qiskit.quantum_info as qi
 
 
-def main(args=None):
-    # Create a Quantum Circuit acting on a quantum register of four qubits
-    qc = QuantumCircuit(4)
-
-    # Initialize the state to binary number 1 (0001)
-    qc.x(0)  # Apply X gate to the least significant qubit
-
-    # Add the binary number 2 (0010) to it
-    qc.cx(0, 1)  # Flip the second least significant qubit if the first qubit is 1
-    qc.x(0)  # Reset the first qubit back to 0
-
-    # Add a barrier to prevent optimization changes
-    qc.barrier()
-
-    # Map the quantum measurement to the classical bits
-    qc.measure_all()
-
-    service = QiskitRuntimeService()
-
-    # Specify a system to use for the noise model
-    real_backend = service.backend("ibm_brisbane")
-    aer = AerSimulator.from_backend(real_backend)
-
-    # Run the sampler job locally using AerSimulator.
-    pm = generate_preset_pass_manager(backend=aer, optimization_level=1)
-    isa_qc = pm.run(qc)
-    sampler = Sampler(backend=aer)
-    result = sampler.run([isa_qc]).result()
-
-    # Extract and Print States
-    measurement_results = result.quasi_dists[0]
-
-    print("Measured States and their Probabilities:")
-    for state, probability in measurement_results.items():
-        print(f"State: {state}, Probability: {probability}")
-
-    # # Drawing the circuit
-    # print("Circuit:")
-    # print(qc.draw(output="text"))
+# Function Definitions
+def get_counts(simulator, circuit, shots=1000):
+    """Return the counts from running a circuit on the Aer"""
+    job_statevector = simulator.run(circuit, shots=shots)
+    counts = job_statevector.result().get_counts(0)
+    return counts
 
 
-if __name__ == "__main__":
-    main()
+def plot_histogram(counts, title="histogram", figsize=(7, 5)):
+    """Plot a histogram of data from a counts dictionary."""
+    if not os.path.exists("results"):
+        os.makedirs("results")
+    labels, values = zip(*sorted(counts.items()))
+    plt.figure(figsize=figsize)
+    plt.bar(labels, values, color="b")
+    plt.xticks(rotation=90)
+    plt.xlabel("State")
+    plt.ylabel("Counts")
+    plt.title(title)
+    plt.savefig(f"results/{title}.png")
+    plt.close()
+
+
+shots = 10000
+simulator = AerSimulator(method="statevector")
+
+# Create a Quantum Circuit acting on a quantum register of four qubits
+qc = QuantumCircuit(4)
+
+# Initialize the state to binary number 1 (0001)
+qc.x(0)  # Apply X gate to the least significant qubit
+
+# Add the binary number 2 (0010) to it
+qc.cx(0, 1)  # Flip the second least significant qubit if the first qubit is 1
+qc.x(0)  # Reset the first qubit back to 0
+
+# Add a barrier to prevent optimization changes
+qc.barrier()
+
+# Map the quantum measurement to the classical bits
+qc.measure_all()
+
+counts = get_counts(simulator, qc, shots=shots)
+print("Final counts:", counts)
+plot_histogram(counts, title="Bell_state_counts_finish")
+print(qc.draw())
